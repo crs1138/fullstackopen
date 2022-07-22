87fd3a5883e3070b30db131f6b4ffb0066cc011e
Jan Honza Pozivil <jpozivil@leadtech.com>
2022-07-22 18:27:24 +0200

    Create a 'Counter' SPA
    Details:
    ---
    * create an app showing how component state works

--- part1/lesson/src/App.js
+++ part1/lesson/src/App.js
@@ -1,24 +1,26 @@
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age;
+import { useState } from "react";
 
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  );
-};
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const Button = ({ text, onClick }) => (
+  <button type="button" onClick={onClick}>
+    {text}
+  </button>
+);
 
 const App = () => {
-  const name = "Peter";
-  const age = 10;
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      <Display counter={counter} />
+      <Button text="plus" onClick={increaseByOne} />
+      <Button text="zero" onClick={setToZero} />
+      <Button text="minus" onClick={decreaseByOne} />
     </div>
   );
 };
--- part1/lesson/src/index.js
+++ part1/lesson/src/index.js
@@ -2,5 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
