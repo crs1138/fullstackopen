8bb5d23cc3812ecdbed6350f84d1f25711a6abfd
Jan Honza Pozivil <jpozivil@leadtech.com>
2022-07-24 16:10:41 +0200

    Refactor Unicafe SPA
    Details:
    ---
    * Change the structure to match the exercise specs.

---
+++ part1/unicafe/src/StatisticsLine.jsx
@@ -0,0 +1,8 @@
+const StatisticsLine = ({ text, value }) => (
+  <tr>
+    <td>{text}:</td>
+    <td>{value}</td>
+  </tr>
+);
+
+export default StatisticsLine;
--- part1/unicafe/src/StatsDisplay.jsx
+++
@@ -1,9 +0,0 @@
-const StatsDisplay = ({ good, neutral, bad }) => (
-  <section>
-    <h2>Stats</h2>
-    <p>good: {good}</p>
-    <p>neutral: {neutral}</p>
-    <p>bad: {bad}</p>
-  </section>
-);
-export default StatsDisplay;
