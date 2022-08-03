2adfdb991cf40cf3998800ca96d89bc092d0b00c
Jan Pozivil <crs1138@me.com>
2022-08-03 19:34:02 +0200

    Add phone number input
    Details:
    ---
    * Exercise 2.8: The Phonebook step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '123-456-789' },
+    ])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
 
     const addPerson = (event) => {
         event.preventDefault()
@@ -14,21 +17,36 @@ const App = () => {
         }
         const personObject = {
             name: newName,
+            number: newNumber,
         }
         setPersons(persons.concat(personObject))
         setNewName('')
+        setNewNumber('')
     }
+
     return (
         <div>
             <h2>Phonebook</h2>
             <form onSubmit={addPerson}>
                 <div>
-                    name:{' '}
+                    <label htmlFor="name">name: </label>
                     <input
                         onChange={(event) => {
                             setNewName(event.target.value)
                         }}
                         value={newName}
+                        id="name"
+                        name="name"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="number">number:</label>
+                    <input
+                        type="text"
+                        name="number"
+                        id="number"
+                        value={newNumber}
+                        onChange={(event) => setNewNumber(event.target.value)}
                     />
                 </div>
                 <div>
@@ -36,9 +54,10 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map((person) => (
-                <p key={person.name}>{person.name}</p>
-            ))}
+            {persons.map((person) => {
+                const { name, number } = person
+                return <p key={name}>{`${name}: ${number}`}</p>
+            })}
         </div>
     )
 }
