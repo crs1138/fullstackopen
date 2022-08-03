f95c07ec69e5e8d12a8c6b52f308719a5ecfca3b
Jan Pozivil <crs1138@me.com>
2022-08-03 18:56:07 +0200

    Create Phonebook SPA
    Details:
    ---
    * Exercise 2.6: The Phonebook Step1

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+const App = () => {
+    const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+    const [newName, setNewName] = useState('')
+
+    const addPerson = (event) => {
+        event.preventDefault()
+        const personObject = {
+            name: newName,
+        }
+        setPersons(persons.concat(personObject))
+        setNewName('')
+    }
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addPerson}>
+                <div>
+                    name:{' '}
+                    <input
+                        onChange={(event) => {
+                            setNewName(event.target.value)
+                        }}
+                        value={newName}
+                    />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map((person) => (
+                <p key={person.name}>{person.name}</p>
+            ))}
+        </div>
+    )
+}
+
+export default App
