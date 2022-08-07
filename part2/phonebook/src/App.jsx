08dce893b448fa7d5db000641bbb643635891e99
Jan Pozivil <crs1138@me.com>
2022-08-07 11:31:31 +0200

    Add server service function that write into the db.json
    Details:
    ---
    * Exercise Part2.15: Phonebook step7
    * Exercise Part2.16: Phonebook step8
    * Exercise Part2.17: Phonebook step9
    * Exercise Part2.18: Phonebook step10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
-import Filter from './Filter'
-import PersonForm from './PersonForm'
-import Persons from './Persons'
+import connectService from './services/connect'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 const App = () => {
     const [persons, setPersons] = useState([])
@@ -11,8 +11,8 @@ const App = () => {
     const [nameFilter, setNameFilter] = useState('')
 
     useEffect(() => {
-        axios.get('http://localhost:3001/persons').then((response) => {
-            setPersons(response.data)
+        connectService.getAll().then((allPersons) => {
+            setPersons(allPersons)
         })
     }, [])
 
@@ -20,26 +20,72 @@ const App = () => {
         setNameFilter(event.target.value)
     }
 
-    const addPerson = (event) => {
-        event.preventDefault()
-        const personExists = !!persons.find((person) => person.name === newName)
-
-        if (personExists) {
-            alert(`${newName} is already added to phonebook`)
-            return
-        }
-        const personObject = {
-            name: newName,
-            number: newNumber,
-        }
-        setPersons(persons.concat(personObject))
-        setNewName('')
-        setNewNumber('')
-    }
-
     const handleNewName = (event) => setNewName(event.target.value)
 
     const handleNewNumber = (event) => setNewNumber(event.target.value)
+
+    const addPerson = (event) => {
+        event.preventDefault()
+        const existingPerson = persons.find((person) => person.name === newName)
+        const isNewPerson = !existingPerson
+
+        let personObject = {
+            name: newName,
+            number: newNumber,
+        }
+
+        if (isNewPerson) {
+            connectService.save(personObject).then((returnedPerson) => {
+                setPersons(persons.concat(returnedPerson))
+                setNewName('')
+                setNewNumber('')
+            })
+        } else {
+            const confirmation = window.confirm(
+                `${newName} already exists in your phonebook\nDo you want to replace their number with a new one?`
+            )
+            if (!confirmation) {
+                return
+            }
+            connectService
+                .update(existingPerson.id, personObject)
+                .then((returnedPerson) => {
+                    setPersons(
+                        persons.map((person) =>
+                            person.id === returnedPerson.id
+                                ? returnedPerson
+                                : person
+                        )
+                    )
+                    setNewName('')
+                    setNewNumber('')
+                })
+        }
+    }
+
+    const removePerson = (event) => {
+        event.preventDefault()
+        const id = Number(event.target.id.replace('button-', ''))
+        const personToBeDeleted = persons.find((person) => person.id === id)
+
+        const confirmation = window.confirm(
+            `Are you sure you want to delete the number of ${personToBeDeleted.name}?\nThis action cannot be undone.`
+        )
+        if (!confirmation) {
+            return
+        }
+        connectService
+            .remove(id)
+            .catch((error) => {
+                console.error(
+                    `The number of ${personToBeDeleted.name} has been already deleted from the server.`
+                )
+                console.error(error)
+            })
+            .finally(() => {
+                setPersons(persons.filter((person) => person.id !== id))
+            })
+    }
 
     const filteredPersons =
         nameFilter === ''
@@ -49,6 +95,7 @@ const App = () => {
                       .toLowerCase()
                       .includes(nameFilter.toLowerCase())
               })
+
     return (
         <div>
             <h1>Phonebook</h1>
@@ -64,7 +111,7 @@ const App = () => {
             />
 
             <h2>Numbers</h2>
-            <Persons persons={filteredPersons} />
+            <Persons persons={filteredPersons} removePerson={removePerson} />
         </div>
     )
 }
