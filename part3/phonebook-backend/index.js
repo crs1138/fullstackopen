987f5307ad3703f582f5043ab33aa5440a7b5d22
Jan Pozivil <crs1138@me.com>
2022-08-08 18:09:37 +0200

    Part3.a: Phonebook exercise 3.4
    Details:
    ---
    * Phonebooks backend step 4: Add DELETE functionality to the server

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -52,6 +52,13 @@ app.get('/api/persons/:id', (req, res) => {
     res.json(person)
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter((person) => person.id !== id)
+
+    res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
