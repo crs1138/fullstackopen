3a65cb71413c1e37418dd7fd66f89a53664bb116
Jan Pozivil <crs1138@me.com>
2022-08-09 10:43:33 +0200

    Part3.a: Phonebook exercise: 3.8
    Details:
    ---
    * Phonebook backend step 8: Configure `morgan` to log body of the POST
      requests

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', function (req, res) {
+    return req.method === 'POST' && JSON.stringify(req.body)
+})
+app.use(
+    morgan(
+        ':method :url :status :res[content-length] - :response-time ms :body'
+    )
+)
 
 let persons = [
     {
