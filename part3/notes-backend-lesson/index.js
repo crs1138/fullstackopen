769a03782513c728b8ffc199039e7b9c61ea95ba
Jan Pozivil <crs1138@me.com>
2022-08-12 12:54:34 +0200

    Part3.c: Saving data to MongoDB - Notes
    Details:
    ---
    * Connect backend to the MongoDB as a separate module
    * Using DB in route handlers

--- part3/notes-backend-lesson/index.js
+++ part3/notes-backend-lesson/index.js
@@ -1,52 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const Note = require('./models/note')
+const { response } = require('express')
 const app = express()
-let notes = [
-    {
-        id: 1,
-        content: 'HTML is easy',
-        date: '2022-1-17T17:30:31.098Z',
-        important: true,
-    },
-    {
-        id: 2,
-        content: 'Browser can execute only JavaScript',
-        date: '2022-1-17T18:39:34.091Z',
-        important: false,
-    },
-    {
-        id: 3,
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        date: '2022-1-17T19:20:14.298Z',
-        important: false,
-    },
-    {
-        content: 'Testing adding new note',
-        date: '2022-08-06T10:27:56.106Z',
-        important: true,
-        id: 4,
-    },
-    {
-        content:
-            'This business of saving new data on the server seems much easier than I thought it would be.',
-        date: '2022-08-06T10:32:24.111Z',
-        important: true,
-        id: 5,
-    },
-    {
-        content: "Here's a new note after refactor",
-        date: '2022-08-06T15:58:18.617Z',
-        important: false,
-        id: 6,
-    },
-    {
-        content:
-            'Refactoring, using the return of the promise.then() for the save functionality',
-        date: '2022-08-06T16:09:14.916Z',
-        important: false,
-        id: 7,
-    },
-]
 
 const requestLogger = (req, res, next) => {
     console.log('Method: ', req.method)
@@ -69,18 +26,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/notes/', (req, res) => {
-    console.log(req.headers)
-    res.json(notes)
+    Note.find({}).then((notes) => {
+        console.log({ notes })
+        res.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const note = notes.find((note) => note.id === id)
-    if (!!note) {
+    Note.findById(req.params.id).then((note) => {
+        console.log({ note })
         res.json(note)
-    } else {
-        res.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/notes/:id', (req, res) => {
@@ -90,24 +46,21 @@ app.delete('/api/notes/:id', (req, res) => {
     res.status(204).end()
 })
 
-const generateId = (nodes) => {
-    const maxId = nodes.length > 0 ? Math.max(...nodes.map((n) => n.id)) : 0
-    return maxId + 1
-}
-
 app.post('/api/notes', (req, res) => {
     const body = req.body
     if (!body.content) {
         return res.status(400).json({ error: `Content missing` })
     }
-    const note = {
+
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId(notes),
-    }
-    notes = notes.concat(note)
-    res.json(note)
+    })
+
+    note.save().then((savedNote) => {
+        res.json(savedNote)
+    })
 })
 
 app.put('/api/notes/:id', (req, res) => {
